1788b12a2509697c8bca4a1ed65797ebf61e86b5
Sandaru Anuththara <sandaruperera108@gmail.com>
2025-06-06 12:13:36 +0530

    add data source and add crosfilters

---
+++ src/main/java/Documents/AAD/JavaEE/Test_Project/CorsFilter.java
@@ -0,0 +1,25 @@
+package Documents.AAD.JavaEE.Test_Project;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/Documents/AAD/JavaEE/Test_Project/SignInServlet.java
@@ -0,0 +1,17 @@
+package Documents.AAD.JavaEE.Test_Project;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/api/v1/SignInServlet")
+public class SignInServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
